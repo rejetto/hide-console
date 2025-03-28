f9bf5f73145112909a98b8e1686d0d7c9d25ebaa
Massimo Melina <a@rejetto.com>
2025-03-28 10:06:49 +0100

    avoid accidentally running 2 instances

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,6 +1,6 @@
 exports.name = "Hide console"
 exports.description = "As soon as this plugin starts, the console window is hidden. Stop plugin to make it appear again."
-exports.version = 1
+exports.version = 1.2
 exports.apiRequired = 1
 exports.repo = "rejetto/hide-console"
 
@@ -9,6 +9,17 @@ exports.repo = "rejetto/hide-console"
 exports.init = api => {
     if (process.platform !== 'win32')
         throw "this plugin only works on Windows"
+    const fs = api.require('fs')
+    const lockName = api.storageDir + 'lock'
+    try { fs.unlinkSync(lockName) }
+    catch(e) {
+        if (e.code !== 'ENOENT') {
+            api.log("quitting because already running")
+            process.exit(0)
+        }
+    }
+    const lockFile = fs.openSync(lockName, 'wx')
+
     const m = require(__dirname + '/node-hide-console-window')
     m.hideConsole()
     return {
@@ -16,6 +27,10 @@ exports.init = api => {
             hideConsole: m.hideConsole,
             showConsole: m.showConsole,
         },
-        unload: m.showConsole,
+        unload() {
+            m.showConsole()
+            fs.closeSync(lockFile)
+            fs.unlinkSync(lockName)
+        },
     }
 }
